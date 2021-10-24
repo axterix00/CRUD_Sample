786d34534e2df4826c07b2517ff1de8ce8bc5575
axterix00 <kurabutosatsugen@gmail.com>
2021-10-25 01:29:33 +0800

    fixed header & footer added body scrolling

--- src/frontend/src/components/Home.js
+++ src/frontend/src/components/Home.js
@@ -4,6 +4,37 @@ import '../resources/css/Home.css';
 export default function Home() {
     return (
         <div className="Home">
+        <h2>Hoome</h2>
+        <h2>Hoome</h2>
+        <h2>Hoome</h2>
+        <h2>Hoome</h2>
+        <h2>Hoome</h2>
+        <h2>Hoome</h2>
+        <h2>Hoome</h2>
+        <h2>Hoome</h2>
+            <h2>Hoome</h2>
+            <h2>Hoome</h2>
+            <h2>Hoome</h2>
+            <h2>Hoome</h2>
+            <h2>Hoome</h2>
+            <h2>Hoome</h2>
+            <h2>Hoome</h2>
+            <h2>Hoome</h2>
+        <h2>Hoome</h2>
+        <h2>Hoome</h2>
+        <h2>Hoome</h2>
+        <h2>Hoome</h2>
+        <h2>Hoome</h2>
+        <h2>Hoome</h2>
+        <h2>Hoome</h2>
+        <h2>Hoome</h2>
+            <h2>Hoome</h2>
+            <h2>Hoome</h2>
+            <h2>Hoome</h2>
+            <h2>Hoome</h2>
+            <h2>Hoome</h2>
+            <h2>Hoome</h2>
+            <h2>Hoome</h2>
             <h2>Hoome</h2>
         </div>
     );
